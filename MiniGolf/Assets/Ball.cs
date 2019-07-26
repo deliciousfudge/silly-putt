5bc36905707b8817a2ca89e7547618347a6140f6
RS <nz.rogersmith@gmail.com>
2019-07-26 17:45:04 +1200

    Added in basic skeleton of Level 1

---
+++ MiniGolf/Assets/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody RB;
+    private Camera Camera;
+    private bool MoveBall = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Get in the hole!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MoveBall)
+        {
+            transform.position += new Vector3(0.001f, 0.001f, 0.025f);
+            transform.Rotate(0.1f, 0.0f, 0.0f);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Hole")
+        {
+            print("Dancin'!");
+
+            MoveBall = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Hole")
+        {
+            print("Dancin'!");
+
+            MoveBall = false;
+        }
+    }
+}
---
+++ MiniGolf/Assets/GameCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+    public GameObject m_Ball;
+
+    private Camera m_Camera;
+    private Vector3 m_v3CameraOffset = new Vector3(0.0f, 0.0f, -5.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Camera.transform.position = m_Ball.transform.position + m_v3CameraOffset;
+        m_Camera.transform.position = new Vector3(m_Camera.transform.position.x, 4.0f, m_Camera.transform.position.z);
+    }
+}
